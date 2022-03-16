c6fb9ab9e03fdcec65d28e10d94f764f3c57caef
Ed Kubiak <ek33450505@gmail.com>
2022-03-16 19:11:24 -0400

    restart/frustrated with orgional code

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,60 +1,11 @@
-var cityInputEl = document.querySelector('#cityForm');
-var cityListEl = document.querySelector('#cityList');
-var searchBtn = document.querySelector('#button-addon'); 
-
-var cityContainerEl = document.querySelector("#cityName");
-var todayContainerEl = document.querySelector("#todayOutput");
-var previousSearch = document.querySelector("#cityHistory");
-var cards = document.querySelector("#card-deck")
+var cityInputEl = document.querySelector('#city-input');
+var cityNameEl = document.querySelector('#city-name');
+var searchBtn = document.querySelector('#search-btn'); 
 
 //Global Variables
-var apiKey = "5332ca0f45c08fd52df6c08ea63d1aa2";
+var apiKey = "eed8624c4362c154ef976f4cb7d4b145";
 var citySearches = [];
 var today = moment().format("L");
 
 // Functions to control form submission
-var submitForm = function (event) {
-    //prevents page from refreshing
-    event.preventDefault();
-    // get value from input element
-    var citySearch = cityInputEl.value.trim();
-    if (citySearch) {
-        getWeather(citySearch);
-        // clear old content
-        cityInputEl.value = '';
-    } else {
-        alert('Please enter a City');
-    }
-    citySearches.push(citySearch);
-    storeCity(citySearches);
-    renderCities();
-};
-//Function to store previous searches in local storage
-var storeCity = function() {
-    localStorage.setItem("searchCity", JSON.stringify(citySearches));
-    return;
-};
-
-var getWeather = function(location) {
-    //format the openweather geocoding api
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=${apiKey}';
-
-    //make a request to the Url
-    fetch(apiUrl)
-        .then(function(response) {
-            //request was sucessful
-            if (response.ok) {
-                // console.log (response);
-                response.json().then(function(currentData) {
-                  displayWeather(currentData, location);
-                  getForcast(currentData, location);
-                });
-            } else {
-                alert('Error: Location not found')
-            }
-        })
-        .catch(function(error) {
-            alert("unable to connect to openweather");
-        });
-};
 
